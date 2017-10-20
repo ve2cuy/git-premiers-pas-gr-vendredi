2bea68f3fde13ab568a5e541290e86019c806089
Alain <alain@LD140-PR-06284.local>
2017-10-20 09:46:57 -0400

    Ajout de "Je suis aussi la ligne 15"

--- git-premiers-pas-gr-vendredi/ViewController.swift
+++ git-premiers-pas-gr-vendredi/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("je suis aussi la ligne 15")
         // Do any additional setup after loading the view, typically from a nib.
         print("viewDidLoad")
     }
