5b62512f36b69f355e1279e4ec58bc7735d712ba
Alain <alain@LD140-PR-06284.local>
2017-10-20 08:53:44 -0400

    Initial Commit

---
+++ git-premiers-pas-gr-vendredi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-premiers-pas-gr-vendredi
+//
+//  Created by Alain on 17-10-20.
+//  Copyright © 2017 Alain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
